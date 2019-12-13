cd4045e1c7eee91bd8854a186cefed7fe0e0fce9
20161104584 <985440246@qq.com>
2019-12-13 16:42:51 +0800

    项目初始化

---
+++ leave/src/main/java/com/student/leave/LeaveApplication.java
@@ -0,0 +1,13 @@
+package com.student.leave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveApplication.class, args);
+    }
+
+}
